a4e71d8daddef2a076c5f67e34573425925aa60b
Justus B <justus.c.brown@gmail.com>
2022-06-28 20:49:55 -0700

    added event listener

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -5,8 +5,14 @@ var secondsLeft = 75;
 //get html element 
 var timerEl = document.querySelector("#timer")  
 
-//grab h4 element
+//grab h2 element
 var qEl =  document.querySelector("#question") 
+
+// grab p element
+var pEl = document.querySelector("#q-subtext") 
+
+// grab q-card element
+var cardEl =  document.querySelector("#q-card") 
 
 //grab all button elements 
 var btns = document.querySelectorAll(".mult-choice-opt")
@@ -40,6 +46,8 @@ var question1 = {
     B: "2. booleans",
     C: "3. alerts",
     D: "4. numbers",
+    p: "", 
+    displayStatus: "display: block",
 }
 
 var question2 = {
@@ -48,9 +56,22 @@ var question2 = {
     B: "2. curly brackets",
     C: "3. parentheses",
     D: "4. square brackets",
+    p: "", 
+    displayStatus: "display: block",
+}
+var question3 = {
+    questionText: "A very useful tol used durong development for printing content to the debugger is: " ,
+    A: "1. JavaScript",
+    B: "2. terminal/bash",
+    C: "3. for loops",
+    D: "4. console.log",
+    p: "", 
+    displayStatus: "display: block",
 }
 
-var qArray = [question1, question2]; //create array of question objects
+
+
+var qArray = [question1, question2, question3]; //create array of question objects
 var qArrayIndex = 0; //init index to 0
 
 function alterQCard() { // alter the contents of the q card
@@ -59,4 +80,18 @@ function alterQCard() { // alter the contents of the q card
     btn2El.textContent = qArray[qArrayIndex].B;
     btn3El.textContent = qArray[qArrayIndex].C;
     btn4El.textContent = qArray[qArrayIndex].D;
+    pEl.textContent =  qArray[qArrayIndex].p;
+    
+    for (i=0; i < btns.length; i++) { //make all buttons display 
+        btns[i].setAttribute("style", qArray[qArrayIndex].displayStatus);
+    }
 }
+
+//set event listener for start button
+cardEl.addEventListener("click", function (event){
+    alterQCard()
+
+    if (qArrayIndex < qArray.length-1){
+        qArrayIndex++
+    }
+})
