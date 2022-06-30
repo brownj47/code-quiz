8166171c722da59c788f26f77dc68dff333049b6
Justus B <justus.c.brown@gmail.com>
2022-06-29 20:08:17 -0700

    styled main, qcard ; added invisble sections; logic to listen for clicks

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -8,6 +8,9 @@ var score = 0;
 //get html element 
 var timerEl = document.querySelector("#timer")  
 
+// get main html el
+var qHeadEl =  document.querySelector("#q-header") 
+
 //grab h2 element
 var qEl =  document.querySelector("#question") 
 
@@ -25,9 +28,19 @@ var btn1El =  document.querySelector("#button-1")
 var btn2El =  document.querySelector("#button-2")
 var btn3El =  document.querySelector("#button-3")
 var btn4El =  document.querySelector("#button-4")
+var subBtn = document.querySelector("#submit-btn")
+var goBackBtn = document.querySelector("#go-back")
+var clearBtn = document.querySelector("#hclear-high-scores")
+
 
 // grab form element
 var formEl = document.querySelector("#submit-form")
+
+//grab high score card element
+var hiScrEl = document.querySelector("#high-score-card")
+
+// grab ol element
+var scoreListEl = document.querySelector("#score-list")
 
 
 function timerSet(){ // a function that decrements the timer counter and writes to the timer element 
@@ -84,7 +97,7 @@ var completedPage = {
     p: `Error`, 
     displayStatus: "display: none",
 }
-
+// foreach 
 var qArray = [question1, question2, question3, ]; //create array of question objects
 var qArray = [question1, question2, question3, completedPage ]; //create array of question objects
 var qArrayIndex = 0; //init index to 0
@@ -96,29 +109,41 @@ function alterQCard() { // alter the contents of the q card
     btn3El.textContent = qArray[qArrayIndex].C;
     btn4El.textContent = qArray[qArrayIndex].D;
     pEl.textContent =  qArray[qArrayIndex].p;
-    
-    for (i=0; i < btns.length; i++) { //make all buttons display 
-        btns[i].setAttribute("style", qArray[qArrayIndex].displayStatus);
-    }
+    cardEl.setAttribute("style", qArray[qArrayIndex].displayStatus)
 }
 
-//set event listener for start button
-cardEl.addEventListener("click", function (event){
 
-    if (event.target === "button") {
-        console.log("yaya")
-    }
-    alterQCard()
-    if (qArrayIndex < qArray.length-1){ // if it is not the last index in the qArray add one to the index
-       qArrayIndex++
-
-    } else{  // if it is the last index in the qArray
-
-        score = secondsLeft //assign a score variable
-        completedPage.p = `Your final score is: ${score}` //alter object to reflect this
-        pEl.textContent = qArray[qArrayIndex].p; // update p element
-        formEl.style = "display: block;" // reveal form element
-
-
+//set event listener for start button\
+qHeadEl.addEventListener("click", function (event) {
+    if (event.target.matches("button")) {
+        alterQCard()
+        qArrayIndex++
+        event.target.setAttribute("style", "display: none;")
     }
 })
+
+
+// event listener for button presses in q-card
+cardEl.addEventListener("click", function (event){
+
+    if (event.target.matches("button")) {
+        alterQCard()
+        if (qArrayIndex < qArray.length-1){ // if it is not the last index in the qArray add one to the index
+           qArrayIndex++
+    
+        } else{  // if it is the last index in the qArray
+    
+            score = secondsLeft //assign a score variable
+            completedPage.p = `Your final score is: ${score}` //alter object to reflect this
+            pEl.textContent = qArray[qArrayIndex].p; // update p element
+            formEl.style = "display: block;" // reveal form element
+        }
+    }
+})
+
+// event listener for the submit button
+subBtn.addEventListener("click", function (event) {
+    event.preventDefault()
+
+}) 
+
